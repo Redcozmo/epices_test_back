5b67c2aaf172d3b5f4b321c0330ac43985930d0d
redcozmo <redcozmo@hotmail.com>
2019-12-17 15:38:12 +0100

    sum method for daily prod

--- app/models/daily_datum.rb
+++ app/models/daily_datum.rb
@@ -1,4 +1,12 @@
 class DailyDatum < ApplicationRecord
   has_many :hourly_data
   belongs_to :inverter
+
+  # day production calculation
+  def sum
+    sum = 0
+    hourly_data.each { |prod| sum += prod.energy }
+    return sum
+  end
+
 end
